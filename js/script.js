e2f8d6ca8758a7b243e97fd10f7d1df2fab48f92
André Brandão <andrebf4@gmail.com>
2016-03-28 21:34:22 -0300

    old - initial struct

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$( document ).ready(function() {
+  $( ".nav-item-carousel" ).click(function() {
+    $( ".nav-item-carousel" ).removeClass("active-item-carousel");
+    $( this ).toggleClass( "active-item-carousel" );
+  });
+});
+
+data = new Date();
+var hyolynAge = data.getFullYear() - 1991;
+var soyouAge = data.getFullYear() - 1992;
+var boraAge = data.getFullYear() - 1990;
+var DasomAge = data.getFullYear() - 1993;
+
+$( ".hyolynAge" ).html(hyolynAge);
+$( ".soyouAge" ).html(hyolynAge);
+$( ".boraAge" ).html(hyolynAge);
+$( ".dasomAge" ).html(hyolynAge);
