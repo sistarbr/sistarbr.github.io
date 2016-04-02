de4c57233068c103ac5fa07cba3dcf21ce5799e2
André Brandão <andrebf4@gmail.com>
2016-04-02 19:02:00 -0300

    view for www

--- js/script.js
+++ js/script.js
@@ -2,9 +2,13 @@ var app = angular.module('sistarBr', ['ngRoute'])
 
   //routes
   .config(function($routeProvider, $locationProvider){
-    $routeProvider.when('/', {
-      templateUrl : 'views/home.html',
-    });
+    $routeProvider
+      .when('/', {
+        templateUrl : 'views/home.html',
+      })
+      .when('/www',{
+        templateUrl: 'views/www.html'
+      })
     $locationProvider.html5Mode({
       enable: true,
       requireBase: false
