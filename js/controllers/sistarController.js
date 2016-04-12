a9229c9b535aedc1951bcc664c74e2a655cab964
André Brandão <andrebf4@gmail.com>
2016-04-12 18:45:26 -0300

    add sentMail controller

--- js/controllers/sistarController.js
+++ js/controllers/sistarController.js
@@ -32,3 +32,54 @@ app.controller('locationController', function($scope, $rootScope, $location){
     return $location.path();
   };
 });
+
+app.controller('sentMail',function($scope, $http){
+  $scope.sendMail = function(a){
+    console.log(a.toEmail);
+    var mailJSON ={
+        "key": "xxxxxxxxxxxxxxxxxxxxxxx",
+        "message": {
+          "html": ""+a.mailBody,
+          "text": ""+a.mailBody,
+          "subject": ""+a.subject,
+          "from_email": "sender@sending.domain.com",
+          "from_name": "Support",
+          "to": [
+            {
+              "email": ""+a.toEmail,
+              "name": "John Doe",
+              "type": "to"
+            }
+          ],
+          "important": false,
+          "track_opens": null,
+          "track_clicks": null,
+          "auto_text": null,
+          "auto_html": null,
+          "inline_css": null,
+          "url_strip_qs": null,
+          "preserve_recipients": null,
+          "view_content_link": null,
+          "tracking_domain": null,
+          "signing_domain": null,
+          "return_path_domain": null
+        },
+        "async": false,
+        "ip_pool": "Main Pool"
+    };
+    var apiURL = "https://mandrillapp.com/api/1.0/messages/send.json";
+    $http.post(apiURL, mailJSON).
+      success(function(data, status, headers, config) {
+        alert('successful email send.');
+        $scope.form={};
+        console.log('successful email send.');
+        console.log('status: ' + status);
+        console.log('data: ' + data);
+        console.log('headers: ' + headers);
+        console.log('config: ' + config);
+      }).error(function(data, status, headers, config) {
+        console.log('error sending email.');
+        console.log('status: ' + status);
+      });
+  }
+})
